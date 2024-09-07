be7522f7065a4a1fe41f325a0415b60487fe6de1
Andrew Wihardja <andrewadi96@gmail.com>
2024-09-07 08:43:22 -0700

    implement generate random phrase in WoFMethods

---
+++ src/main/java/WheelOfFortuneMethods.java
@@ -0,0 +1,9 @@
+public class WheelOfFortuneMethods {
+    public static void generateRandomPhrase() {
+        // Implement generate phrase
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
